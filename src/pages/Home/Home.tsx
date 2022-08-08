637733fae61a7df0002b6a3b320c4f9fdff0ffdf
Rabie Menad <rabie.menad07@outlook.com>
2022-08-08 21:59:22 +0200

    Set up the Home Page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,19 @@
 import styles from "./Home.module.scss";
-
+import WorkingImg from "../../images/illustration-working.svg";
 
 export default function Home() {
   return (
     <div className={styles.home_container}>
-      Home
+      
+      <div className={styles.bloc_img}>
+         <img src={WorkingImg} alt="working illustration" />
+      </div>
+
+      <div className={styles.home_content}>
+         <h1>More than just shorter links</h1>
+         <p>Build your brand’s recognition and get detailed insights on how your links are performing.</p>
+         <button className={styles.call_to_action}></button>
+      </div>
     </div>
   )
 }
