5939683a97746d1720ffb0897038e2739d77db25
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-08-10 16:06:39 +0200

    Implementation of the header service

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -2,6 +2,20 @@ import styles from "./Services.module.scss";
 
 export default function Services() {
   return (
-    <div className={styles.services_container}> Services</div>
+    <div className={styles.services_container}> 
+
+    <div className={styles.services_header}>
+      <input type="text" 
+             name="shorten-link"     
+             placeholder="Shorten a link here..." />
+      <button>Shorten it!</button>
+    </div>
+
+    <div className={styles.services_informations}>
+      <h3>Advanced Statistics</h3>
+      <p>Track how your links are performing across the web with our advanced statistics dashboard</p>
+    </div>
+      
+    </div>
   )
 }
