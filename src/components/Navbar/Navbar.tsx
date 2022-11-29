acf9a393fc61acb0b10b92f196e9f2b9c0fbb032
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-11-29 21:47:06 +0100

    Adding animations on the navbar and home page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,44 +1,101 @@
 import styles from "./Navbar.module.scss";
 import logo from "../../images/logo.svg";
 import { useState } from "react";
-import cx from 'classnames';
+import cx from "classnames";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-
+import { motion } from "framer-motion";
 
 export default function Navbar() {
-
-   const [isActive, setIsActive] = useState(false)
-   const toggleModal = () => setIsActive(!isActive)
+  const [isActive, setIsActive] = useState(false);
+  const toggleModal = () => setIsActive(!isActive);
 
   return (
     <nav>
       <div className={styles.navbar_container}>
+        <div className={styles.logo_container}>
+          <motion.img
+            src={logo}
+            alt="logo"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3 }}
+          />
+        </div>
 
-         <div className={styles.logo_container}>
-            <img src={logo} alt="logo" />
-         </div>
+        <div
+          className={
+            isActive
+              ? cx(styles.nav_content, styles.active)
+              : styles.nav_content
+          }
+        >
+          <ul
+            className={
+              isActive ? cx(styles.links, styles.active) : styles.links
+            }
+          >
+            <li>
+              <motion.a
+                href="#features"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.3, delay: 0.1 }}
+              >
+                Features
+              </motion.a>
+            </li>
+            <li>
+              <motion.a
+                href="#pricing"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.3, delay: 0.2 }}
+              >
+                {" "}
+                Pricing
+              </motion.a>
+            </li>
+            <li>
+              <motion.a
+                href="#ressources"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.3, delay: 0.3 }}
+              >
+                Ressources
+              </motion.a>
+            </li>
+          </ul>
 
-         <div className={isActive ? cx(styles.nav_content, styles.active)
-                                  : styles.nav_content}>
-            
-            <ul className={isActive ? cx(styles.links, styles.active)  : styles.links}>
-               <li><a href="#features">Features</a></li>
-               <li><a href="#pricing"> Pricing</a></li> 
-               <li><a href="#ressources">Ressources</a></li>
-            </ul>
+          <div className={styles.white_line}></div>
 
-            <div className={styles.white_line}></div>
+          <div className={styles.btn_container}>
+            <motion.button
+              className={styles.login_btn}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.3, delay: 0.4 }}
+            >
+              Login
+            </motion.button>
+            <motion.button
+              className={styles.call_to_action}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.3, delay: 0.6 }}
+            >
+              Sign Up
+            </motion.button>
+          </div>
+        </div>
 
-            <div className={styles.btn_container}>
-               <button className={styles.login_btn}>Login</button>
-               <button className={styles.call_to_action}>Sign Up</button>
-            </div>
-         </div>
-
-         <div className={styles.hamburger} onClick={toggleModal}>
-            {isActive ? <AiOutlineClose /> : <AiOutlineMenu /> }
-         </div>
-
+        <div className={styles.hamburger} onClick={toggleModal}>
+          {isActive ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </div>
       </div>
     </nav>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,26 +1,40 @@
 import styles from "./Home.module.scss";
 import WorkingImg from "../../images/illustration-working.svg";
-
+import { motion } from "framer-motion";
 
 export default function Home() {
-
   return (
     <div className={styles.home_container}>
       <div className={styles.home_content}>
+        <motion.div
+          className={styles.bloc_img}
+          initial={{ x: -200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.1 }}
+        >
+          <img src={WorkingImg} alt="working illustration" />
+        </motion.div>
 
-         <div className={styles.bloc_img}>
-            <img src={WorkingImg} alt="working illustration" />
-         </div>
-
-         <div className={styles.home_description}>
-            <h1>More than just shorter links</h1>
-            <p>Build your brand’s recognition and get detailed insights on how your links are performing.</p>
-            <button>
-              <a href="#ressources">Get Started</a>
-            </button>
-         </div>
-
+        <motion.div
+          className={styles.home_description}
+          initial={{ x: -400, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.2 }}
+        >
+          <h1>More than just shorter links</h1>
+          <p>
+            Build your brand’s recognition and get detailed insights on how your
+            links are performing.
+          </p>
+          <motion.a
+            href="#ressources"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            Get Started
+          </motion.a>
+        </motion.div>
       </div>
     </div>
-  )
+  );
 }
