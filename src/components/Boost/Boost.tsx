472e0542f52b7446bce30c53e66490a2644a9bb9
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-11-30 19:56:25 +0100

    Setting a bit of animations

--- src/components/Boost/Boost.tsx
+++ src/components/Boost/Boost.tsx
@@ -1,9 +1,16 @@
 import styles from "./Boost.module.scss";
 import { motion } from "framer-motion";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Boost = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
-    <div className={styles.boost_container} id="pricing">
+    <div className={styles.boost_container} id="pricing" data-aos="zoom-in">
       <div className={styles.boost_content}>
         <h2>Boost your links today</h2>
         <motion.button whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -1,10 +1,12 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { fetchUrl } from "../../api/shortener";
 import { ShortURL } from "../../types/ShortURL";
 import styles from "./Services.module.scss";
 import Service from "../../components/Services/CardList/CardList";
 import Links from "../../components/Services/Links/Links";
 import { motion } from "framer-motion";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Services() {
   const [links, setLinks] = useState<ShortURL[]>([]);
@@ -18,11 +20,16 @@ export default function Services() {
     fetchUrl(values.link).then((value) => setLinks([...links, value.data]));
   };
 
+  useEffect(() => {
+    AOS.init({ duration: 1500 });
+  }, []);
+
   return (
     <div className={styles.services_container} id="features">
       <div className={styles.services_content}>
         <form onSubmit={handleSubmit}>
           <input
+            data-aos="flip-down"
             type="url"
             pattern="http://.*"
             name="link"
